a35bdef9dd9ba578c5730986ff5a10c501a68b03
Ajit Dhivar <ajitdhivar4d@gmail.com>
2024-08-14 15:46:37 +0530

    update

--- src/pages/Products/ProductCard.tsx
+++ src/pages/Products/ProductCard.tsx
@@ -1,6 +1,7 @@
 import { AiOutlineShoppingCart } from "react-icons/ai";
 import { Link } from "react-router-dom";
 import HeartIcon from "./HeartIcon";
+import { FaLongArrowAltRight } from "react-icons/fa";
 
 interface Product {
   _id: string;
@@ -55,26 +56,12 @@ const ProductCard = () => {
 
         <section>
           <Link to={`/product/${p._id}`} className="link-readMore">
-            Read More
-            <svg
-              className="w-3.5 h-3.5 ml-2"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 14 10"
-            >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M1 5h12m0 0L9 1m4 4L9 9"
-              />
-            </svg>
+            Read More...
+            <FaLongArrowAltRight size={20} />
           </Link>
 
           <button onClick={() => addToCartHandler()}>
-            <AiOutlineShoppingCart size={25} />
+            <AiOutlineShoppingCart size={20} />
           </button>
         </section>
       </div>
