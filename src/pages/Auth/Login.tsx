5848c58dadb4e0bdaecdeee90693d2cc6d82396a
Ajit Dhivar <ajitdhivar4d@gmail.com>
2024-08-14 19:21:18 +0530

    update

--- src/pages/Auth/Login.tsx
+++ src/pages/Auth/Login.tsx
@@ -1,5 +1,58 @@
+import { FormEvent, useState } from "react";
+import { Link } from "react-router-dom";
+
 const Login = () => {
-  return <div>Login</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const submitHandler = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("submitHandler");
+    console.log("Email", email);
+    console.log("Password", password);
+  };
+
+  return (
+    <div className="login-container">
+      <section>
+        <div className="login-form">
+          <h1>Sign In</h1>
+          <form onSubmit={submitHandler}>
+            <div className="email">
+              <label htmlFor="email">Email Address</label>
+              <input
+                type="email"
+                id="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div className="password">
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                id="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <button type="submit">Sign In</button>
+          </form>
+
+          <div>
+            <p>
+              New Customer ? {""}
+              <Link to="/register">Register</Link>
+            </p>
+          </div>
+        </div>
+        <img
+          src="https://images.unsplash.com/photo-1618005182384-a83a8bd57fbe?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1964&q=80"
+          alt=""
+        />
+      </section>
+    </div>
+  );
 };
 
 export default Login;
