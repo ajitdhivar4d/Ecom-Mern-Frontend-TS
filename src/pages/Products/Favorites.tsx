88954b22b2e41064ffb3c90c67ad4edb8f869e11
Ajit Dhivar <ajitdhivar4d@gmail.com>
2024-08-14 22:53:10 +0530

    update

--- src/pages/Products/Favorites.tsx
+++ src/pages/Products/Favorites.tsx
@@ -1,5 +1,17 @@
+import Product from "./Product";
+
 const Favorites = () => {
-  return <div>Favorites</div>;
+  return (
+    <div className="favorites-container">
+      <h1>FAVORITE PRODUCTS</h1>
+      <div className="favorites-items">
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+      </div>
+    </div>
+  );
 };
 
 export default Favorites;
