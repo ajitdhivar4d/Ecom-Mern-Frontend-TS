c4d5adfeaad7cb4d4f6252ccbbd6394ef75f1038
Ajit Dhivar <ajitdhivar4d@gmail.com>
2024-08-25 15:23:07 +0530

    update

--- src/pages/Products/Favorites.tsx
+++ src/pages/Products/Favorites.tsx
@@ -1,14 +1,12 @@
-import Product from "./Product";
-
 const Favorites = () => {
   return (
     <div className="favorites-container">
       <h1>FAVORITE PRODUCTS</h1>
       <div className="favorites-items">
+        {/* <Product />
         <Product />
         <Product />
-        <Product />
-        <Product />
+        <Product /> */}
       </div>
     </div>
   );
