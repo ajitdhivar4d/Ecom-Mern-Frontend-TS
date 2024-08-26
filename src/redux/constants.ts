f4ef812cd66bf7315e317b0dc3dbe3cb2e7f2d71
Ajit Dhivar <ajitdhivar4d@gmail.com>
2024-08-26 06:44:43 +0530

    update

--- src/redux/constants.ts
+++ src/redux/constants.ts
@@ -1,4 +1,15 @@
-export const server = import.meta.env.VITE_SERVER as string;
+const rawServer = import.meta.env.VITE_SERVER;
+
+let server: string;
+
+if (isValidServerUrl(rawServer)) {
+  server = rawServer;
+} else {
+  // Handle the invalid server URL scenario
+  // You can throw an error, use a default value, or log a warning
+  console.error("Invalid server URL provided. Using default server URL.");
+  server = "https://ecom-mern-backend-ts.onrender.com";
+}
 
 export const BASE_URL = `${server}`;
 
@@ -13,3 +24,7 @@ export const UPLOAD_URL = `${server}/api/upload`;
 export const ORDERS_URL = `${server}/api/orders`;
 
 export const PAYPAL_URL = `${server}/api/config/paypal`;
+
+function isValidServerUrl(value: any): value is string {
+  return typeof value === "string" && value.trim() !== "";
+}
